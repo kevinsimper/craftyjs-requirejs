1d0d8d833321e54d3d0a65b88fd18d183d19a7b1
Kevin Simper <kevin.simper@gmail.com>
2014-01-15 23:14:42 +0100

    Add a background

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 define(['app/bower_components/crafty/dist/crafty'], function(crafty){
   Crafty.init(300, 300);
 
+  Crafty.background('black');
+
   var box = Crafty.e('2D, Canvas, Color')
     .attr({
       x: 10, y: 10, w: 100, h: 100
